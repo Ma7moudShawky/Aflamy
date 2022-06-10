4ae305eae285f305756542635d471aa76fa3f5d9
Ali Shawky Mohamed <ali.shawky.iti@gmail.com>
2022-06-10 15:33:52 +0200

    Clean Project

--- Aflamy/Controllers/MoviesController.cs
+++ Aflamy/Controllers/MoviesController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Aflamy.Models;
+﻿using Aflamy.Models;
 using Aflamy.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Aflamy.Controllers
 {
